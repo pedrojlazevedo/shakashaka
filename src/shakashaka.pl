e374d8e2c5441a34fcd9d7943c03d177c6c7af9c
Pedro Azevedo <joao.seta4ever@gmail.com>
2018-12-19 03:33:56 +0000

    Fixed tons of bugs. Almost finished the restriction of making rectangles with inserting triangles

--- src/shakashaka.pl
+++ src/shakashaka.pl
@@ -156,7 +156,7 @@ solve_puzzle(Puzzle, Solution) :-
 
   true.
 
-make_sum_one([], [], [], [], [], []) :- !.
+make_sum_one([], [], [], [], [], []).
 make_sum_one([L1 | L1s], [L2 | L2s], [L3 | L3s], [L4 | L4s], [L5 | L5s], [P | Ps]) :-
   P = 'e',
   Sum is 1,
@@ -180,26 +180,32 @@ make_sum_equals_puzzle(L1, L2, L3, L4, L5, P, X, Y, XMax, YMax) :-
 
 make_sum_equals_puzzle_r(         _,          _,          _,          _,      _, _, _, 0,    _,    _) :- !.
 make_sum_equals_puzzle_r(TriangleNL, TriangleNR, TriangleSL, TriangleSR, Whites, P, X, Y, XMax, YMax) :-
+  
   YNew is Y - 1,
   get_value(P, X, Y, R),
-  
-  R \= 'e',
+  R \= 'e', !,
 
+  get_value(TriangleNL, X, Y, R1),
+  get_value(TriangleNR, X, Y, R2),
+  get_value(TriangleSL, X, Y, R3),
+  get_value(TriangleSR, X, Y, R4),
+  get_value(Whites    , X, Y, R5),
+  
+  sum([R1, R2, R3, R4, R5], #=, 0),
+  
   %up position
   XUP is X - 1,
   get_value(TriangleNL, XUP, Y, R1UP),
   get_value(TriangleNR, XUP, Y, R2UP),
   get_value(TriangleSL, XUP, Y, R3UP),
   get_value(TriangleSR, XUP, Y, R4UP),
-  get_value(Whites    , XUP, Y, R5UP),
   
   %this up position only can have blank or llt or lrt
   (   XUP > 0	->
-     sum([R3UP, R4UP, R5UP], #=, 1)
+     sum([R1UP, R2UP], #=, 0)
 	 ; true %out of bounderies
 			% if needed we can put that when they are at the borders XUP = 1 that something is not permitted
-  ),
-  
+  ),  
   
   %left
   YL is Y - 1,
@@ -207,9 +213,8 @@ make_sum_equals_puzzle_r(TriangleNL, TriangleNR, TriangleSL, TriangleSR, Whites,
   get_value(TriangleNR, X, YL, R2L),
   get_value(TriangleSL, X, YL, R3L),
   get_value(TriangleSR, X, YL, R4L),
-  get_value(Whites    , X, YL, R5L),
   (   YL > 0	->
-     sum([R2L, R4L, R5L], #=, 1)
+     sum([R1L, R3L], #=, 0)
 	 ; true %out of bounderies
   ),
 
@@ -219,9 +224,8 @@ make_sum_equals_puzzle_r(TriangleNL, TriangleNR, TriangleSL, TriangleSR, Whites,
   get_value(TriangleNR, XD, Y, R2D),
   get_value(TriangleSL, XD, Y, R3D),
   get_value(TriangleSR, XD, Y, R4D),
-  get_value(Whites    , XD, Y, R5D),
   (   XD < XMax	->
-     sum([R1D, R2D, R5D], #=, 1)
+     sum([R3D, R4D], #=, 0)
 	 ; true %out of bounderies
   ),
 
@@ -231,9 +235,8 @@ make_sum_equals_puzzle_r(TriangleNL, TriangleNR, TriangleSL, TriangleSR, Whites,
   get_value(TriangleNR, X, YR, R2R),
   get_value(TriangleSL, X, YR, R3R),
   get_value(TriangleSR, X, YR, R4R),
-  get_value(Whites    , X, YR, R5R),
   (   YR < YMax	->
-     sum([R1R, R3R, R5R], #=, 1)
+     sum([R2R, R4R], #=, 0)
     ; true %out of bounderies
   ),
 
@@ -254,6 +257,17 @@ make_sum_equals_puzzle_r(TriangleNL, TriangleNR, TriangleSL, TriangleSR, Whites,
   get_value(P, X, Y, R),
   R == 'e', %confirmacao que e so em espacos brancos
   
+  %if they are in borders they can't make 45º degree  
+  get_value(TriangleNL, X, Y, R1),
+  get_value(TriangleNR, X, Y, R2),
+  get_value(TriangleSL, X, Y, R3),
+  get_value(TriangleSR, X, Y, R4),
+	
+  (X == 1 ->    sum([R3, R4], #=, 0);true),
+  (Y == 1 ->    sum([R2, R4], #=, 0);true),
+  (Y == YMax -> sum([R1, R3], #=, 0);true),
+  (X == XMax -> sum([R1, R2], #=, 0);true), 
+  
   %right
   YR is Y + 1,
   %down
@@ -262,23 +276,27 @@ make_sum_equals_puzzle_r(TriangleNL, TriangleNR, TriangleSL, TriangleSR, Whites,
   YL is Y - 1,
   %up position
   XUP is X - 1, 
+
   
-  %if they are in borders they can't make 45º degree  
-  get_value(TriangleNL, X, Y, R1),
-  get_value(TriangleNR, X, Y, R2),
-  get_value(TriangleSL, X, Y, R3),
-  get_value(TriangleSR, X, Y, R4),
-  get_value(Whites    , X, Y, R5),
-	
-  (X == 1 ->    sum([R3, R4], #=, 0);true),
-  (Y == 1 ->    sum([R2, R4], #=, 0);true),
-  (Y == YMax -> sum([R1, R3], #=, 0);true),
-  (X == XMax -> sum([R1, R2], #=, 0);true), 
+  %NL triangles (R1)
+	%down direction
+	  get_value(TriangleNL, XD, YL, NLD1),
+	  get_value(TriangleSL, XD, Y, NLD2),
+	  R1 #=< NLD1 + NLD2,
+	%up direction
+	  get_value(TriangleSL, XUP, YR, NLU1),
+	  get_value(TriangleNR, X, YR, NLU2),
+	  R1 #=< NLU1 + NLU2,
+	  
+  %SL triangles (R3)
+	%down direction
+	  get_value(TriangleSL, XD, YR, SLD1),
+	  get_value(TriangleSR, X, YR, SLD2),
+	  R3 #=< SLD1 + SLD2,
+	%up direction
+	  get_value(TriangleSL, XUP, YL, SLU1),
+	  get_value(TriangleNL, XUP, Y, SLU2),
+	  R3 #=< SLU1 + SLU2, 
   
   make_sum_equals_puzzle_r(TriangleNL, TriangleNR, TriangleSL, TriangleSR, Whites, P, X, YNew, XMax, YMax).
-	
-
-make_sum_equals_puzzle_r(  L1, L2, L3, L4, L5, P, X,    Y, XMax, YMax) :-
-  YNew is Y - 1,
-  make_sum_equals_puzzle_r(L1, L2, L3, L4, L5, P, X, YNew, XMax, YMax).
 
